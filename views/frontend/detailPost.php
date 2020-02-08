bd5dd079cb2094d0fccb74607fb7ce6b8d9bfc0d
Antoine <a.heidet90400@gmail.com>
2020-02-08 16:12:14 +0100

    mise en place front pour detailPost

--- views/frontend/detailPost.php
+++ views/frontend/detailPost.php
@@ -1,45 +1,51 @@
 <?php $title = htmlspecialchars($post['title']); ?>
 
 <?php ob_start(); ?>
-<h1>Mon super blog !</h1>
-<p><a href="index.php">Retour � la liste des billets</a></p>
 
-<div class="news">
-    <h3>
-        <?= htmlspecialchars($post['title']) ?>
-        <em>le <?= $post['date'] ?></em>
-    </h3>
-    
-    <p>
-        <?= nl2br(htmlspecialchars($post['content'])) ?>
+<div class="container" style="margin-top: 15px;" id="page_chapitre">
+    <p class="bouton_retour pt3">
+        <a href="shttp://localhost:8888/">
+            <button class="btn btn-info">Retour</button>
+        </a>
     </p>
-</div>
 
-<h2>Commentaires</h2>
+    <div class="card mb-4">
+        <div class="card-body">
+            <h2 class="card-title"><?= htmlspecialchars($post['title']) ?></h2>
+            <p class="card-text"><?= nl2br(htmlspecialchars($post['content'])) ?></p>
+        </div>
+        <div class="card-footer text-muted"><?= $post['date'] ?></div>
+    </div>
 
-<form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-    <div>
-        <label for="fullname">Auteur</label><br />
-        <input type="text" id="fullname" name="fullname" />
+    <?php foreach ($comments as $comment): ?><!-- parcourir les comment  -->
+    <div class="media mb-4">
+        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+        <div class="media-body">
+            <h5 class="mt-0"><?= htmlspecialchars($comment['fullname']) ?></h5> <!-- afficher le nom  -->
+                <p><?= nl2br(htmlspecialchars($comment['content'])) ?></p> <!-- parcourir le contenu  -->
+                <p>Poster le : <date><?= $comment['date'] ?></date></p><!-- parcourir les date  -->
+        </div>
     </div>
-    <div>
-        <label for="content">Commentaire</label><br />
-        <textarea id="content" name="content"></textarea>
-    </div>
-    <div>
-        <input type="submit" />
-    </div>
-</form>
+    <?php endforeach; ?><!-- stop parcourir -->
 
-<?php
-while ($comment = $comments->fetch())
-{
-?>
-    <p><strong><?= htmlspecialchars($comment['fullname']) ?></strong> le <?= $comment['date'] ?></p>
-    <p><?= nl2br(htmlspecialchars($comment['content'])) ?></p>
-<?php
-}
-?>
+
+    <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+            <h2>Commentaires</h2>
+            <div class="form-group">
+                <label for="fullname">Nom : </label>
+                <input type="text" id="fullname" name="fullname" class="form-control input-sm" />
+            </div>
+            <div class="form-group">
+                <label for="content">Commentaire :</label>
+                <textarea id="content" name="content" class="form-control input-sm"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-success" />
+            </div>
+        </form>
+    </div>
+
+
 <?php $contentPage = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
